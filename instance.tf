7c09a5c962cc0b0b95ea33acb225ae9288dedc17
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-10 18:34:37 +0530

    this is resource file to allow 80 port

--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "myserver" {
     ami = "ami-0e2c8caa4b6378d8c"
     key_name = "shell-keynew"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["sg-0004cad9b30fd750c"]
+    vpc_security_group_ids = ["sg-0b66cddd21554c10c"]
     availability_zone = "us-east-1a"
     tags = {
         name = "vishal"
@@ -18,5 +18,6 @@ resource "aws_instance" "myserver" {
               systemctl start apache2
               systemctl enable apache2
               EOF
+
   
 }
