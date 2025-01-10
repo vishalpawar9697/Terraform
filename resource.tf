a289efbda165f268db3a09719a0a266aa0e4df57
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-10 18:22:41 +0530

    this is resource file to allow 80 port

---
+++ resource.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "launch-wizard-1" {
+  name        = "launch-wizard-1"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
