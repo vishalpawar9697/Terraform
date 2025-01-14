550c50d37fccb00017891922ae94c345e1aa810d
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-14 23:02:50 +0530

    this is lauch instance using terrafrom

--- instance.tf
+++ instance.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "myinstance" {
     ami = "ami-0e2c8caa4b6378d8c"
     key_name = "shell-keynew"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [ sg-0004cad9b30fd750c ]
+    vpc_security_group_ids = [ "sg-0004cad9b30fd750c" ]
     tags = {
         name = "myinstance"
     }
