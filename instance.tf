deefea7b3ef4a38dfd078859bf5878c2cf28392f
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-14 22:54:53 +0530

    this is lauch instance using terrafrom

--- instance.tf
+++ instance.tf
@@ -1,23 +1,18 @@
+#block_type resource_type resource_name {
+#configration files
+#}
+
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east1"
 }
-resource "aws_instance" "myserver" {
+
+resource "aws_instance" "myinstance" {
     ami = "ami-0e2c8caa4b6378d8c"
     key_name = "shell-keynew"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["sg-0b66cddd21554c10c"]
-    availability_zone = "us-east-1a"
+    vpc_security_group_ids = [sg-0004cad9b30fd750c]
     tags = {
-        name = "vishal"
-        env = "dev"
+        name = "myinstance"
     }
-    user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              systemctl start apache2
-              systemctl enable apache2
-              EOF
-
   
 }
