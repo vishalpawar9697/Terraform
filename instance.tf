4bf2ebc68457c2d09a19f83eefd6f3465f2de353
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-14 23:34:03 +0530

    this is lauch instance using terrafrom

--- instance.tf
+++ instance.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "myinstance" {
     key_name = "shell-keynew"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["aws_security_group.mysg.id"]
-    depends_on = [ aws_security_group.mys ]
+    depends_on = [ aws_security_group.mysg ]
 
     tags = {
         name = "vishal-instance"
@@ -20,22 +20,21 @@ resource "aws_instance" "myinstance" {
 }
 
 resource "aws_security_group" "mysg" {
-    name = "devopsSg"
-    description = "Allow HTTP traffic"
-    vpc_id = "vpc-0d0f87f5d80c69ffa"
+  name        = "devops-sg"
+  description = "Allow HTTP traffic"
+  vpc_id = "vpc-0d0f87f5d80c69ffa"
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    }
-    
-    egress = {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
