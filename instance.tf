d425972287c3eb05589e863a480657262965faf0
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-14 23:44:05 +0530

    this is lauch instance using terrafrom

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "new_instance" {
+resource "aws_instance" "devops_instance" {
     ami = "ami-0e2c8caa4b6378d8c"
     key_name = "shell-keynew"
     instance_type = "t2.micro"
