f593faea0c1ee5107198f43def224fe2a1128286
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-15 11:05:20 +0530

    this is instance file using terraform

--- instance.tf
+++ instance.tf
@@ -13,10 +13,10 @@ resource "aws_instance" "newinstance" {
     vpc_security_group_ids = [ aws_security_group.new_sg.id ]
     user_data              = <<-EOF
                           #!/bin/bash
-                          sudo yum update -y
-                          sudo yum install -y java-1.8.0-openjdk
-                          sudo yum install -y wget
-                          wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz
+                          sudo apt-get update -y
+                          sudo apt install -y java-1.8.0-openjdk
+                          sudo apt install -y wget
+                          curl -O https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz
                           tar -xzf apache-tomcat-9.0.41.tar.gz
                           sudo mv apache-tomcat-9.0.41 /usr/local/tomcat9
                           sudo chmod +x /usr/local/tomcat9/bin/*.sh
