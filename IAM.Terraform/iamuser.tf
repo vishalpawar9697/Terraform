52305f6f842a8745d5c260947f44f2aa6c96a659
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-15 17:41:24 +0530

    iam user terraform file

--- IAM.Terraform/iamuser.tf
+++ IAM.Terraform/iamuser.tf
@@ -9,7 +9,7 @@ resource "aws_iam_user" "munna" {
 
 resource "aws_iam_user_login_profile" "munna_login_profile" {
     user = "aws_iam_user.munna.name"
-    password = "munna@123"
+    password = "munna@123!"
     password_reset_required = false
   
 }
