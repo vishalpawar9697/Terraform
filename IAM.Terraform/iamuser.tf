73d97b6e9714a672d2e0928b80971e200b97b500
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-15 18:26:44 +0530

    iam user terraform file

--- IAM.Terraform/iamuser.tf
+++ IAM.Terraform/iamuser.tf
@@ -17,9 +17,9 @@ resource "aws_iam_group" "Testing" {
     name = "Testing"
 }
 
-resource "aws_iam_group_policy_attachment" "Testing1_permission" {
-    group = "aws_iam_group.Testing.name"
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-    depends_on = [aws_iam_group.Testing]
+resource "aws_iam_group_policy_attachment" "Testing_permission" {
+  group      = aws_iam_group.Testing.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  depends_on = [aws_iam_group.Testing]
   
 }
