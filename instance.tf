a2c6799a7d18c359cabfa682175db12d3a44f8b8
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-15 10:32:29 +0530

    this is instance file using terraform

--- instance.tf
+++ instance.tf
@@ -10,8 +10,7 @@ resource "aws_instance" "myserver" {
     ami = "ami-01816d07b1128cd2d"
     key_name = "shell-keynew" 
     instance_type = "t2.micro"
-    vpc_security_group_ids = aws_security_group.my_sg.id
-    depends_on = [ aws_security_group.my_sg ]
+    vpc_security_group_ids = [ "aws_security_group.my_sg.id" ]
     availability_zone = "us-east-1"
     tags = {
       name = "vishal"
