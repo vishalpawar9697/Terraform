a6fe3fbc8dead9fea69dfad9fe6a8b5926583ed4
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-15 18:12:41 +0530

    iam user terraform file

--- IAM.Terraform/iamuser.tf
+++ IAM.Terraform/iamuser.tf
@@ -10,5 +10,15 @@ resource "aws_iam_user" "munna" {
 resource "aws_iam_user_policy_attachment" "munna_permission" {
     user = aws_iam_user.munna.name
     policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+    
+}
+
+resource "aws_iam_group" "Testing" {
+    name = "Testing"
+}
+
+resource "aws_iam_group_policy_attachment" "Testing_permission" {
+    group = "aws_iam_group.Testing.name"
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
   
 }
