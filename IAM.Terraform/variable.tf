6b3216958b05f8e79fa7af1fd428ac534c8d104c
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-15 19:19:07 +0530

    multiple i am user created by using terraform

--- IAM.Terraform/variable.tf
+++ IAM.Terraform/variable.tf
@@ -1,4 +1,9 @@
 # Define a list of user names
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 variable "user_names" {
   type    = list(string)
   default = ["vijay", "rajiv", "kalin"]  # List of multiple IAM user names
