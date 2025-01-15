9b4beb912359559bcc8a371cd66f0a492ee1f528
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-15 10:52:43 +0530

    this is instance file using terraform

--- instance.tf
+++ instance.tf
@@ -11,6 +11,18 @@ resource "aws_instance" "newinstance" {
     key_name = "shell-keynew" 
     instance_type = "t2.micro"
     vpc_security_group_ids = [ aws_security_group.new_sg.id ]
+    user_data              = <<-EOF
+                          #!/bin/bash
+                          sudo yum update -y
+                          sudo yum install -y java-1.8.0-openjdk
+                          sudo yum install -y wget
+                          wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz
+                          tar -xzf apache-tomcat-9.0.41.tar.gz
+                          sudo mv apache-tomcat-9.0.41 /usr/local/tomcat9
+                          sudo chmod +x /usr/local/tomcat9/bin/*.sh
+                          sudo /usr/local/tomcat9/bin/startup.sh
+                          EOF
+                    
     tags = {
       Name = "vishal-instance"
       env = "dev"
