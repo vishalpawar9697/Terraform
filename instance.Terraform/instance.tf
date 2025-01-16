3338a5ff87ccf25b3808b577cf599865d2863331
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-17 00:12:48 +0530

    this is launch instance with apache tomcat with terraform file

--- instance.Terraform/instance.tf
+++ instance.Terraform/instance.tf
@@ -24,6 +24,13 @@ resource "aws_security_group" "new_sg" {
   }
 
   ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
   from_port   = 8080
   to_port     = 8080
   protocol    = "tcp"
