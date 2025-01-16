a70cf9233821ef3092f0844768b7ac51d72c0f40
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-17 00:06:05 +0530

    this is launch instance with apache tomcat with terraform file

--- instance.Terraform/variables.tf
+++ instance.Terraform/variables.tf
@@ -1,14 +1,17 @@
 variable "ami_id" {
   description = "The ID of the AMI to use"
-  type        =  "ami-0e2c8caa4b6378d8c"
+  type        = string
+  default = "ami-0e2c8caa4b6378d8c"
 }
 variable "key_name" {
     description = "this is private key"
-    type = "shell-keynew"
+    type = string
+    default = "shell-keynew"
 }
 variable "instance_type" {
     description = "Type of ec2 instance"
-    type = "t2.micro"
+    type = string
+    default = "t2.micro"
 }
 variable "region" {
   description = "The AWS region to create resources in"
@@ -18,6 +21,7 @@ variable "region" {
 
 variable "vpc_id" {
     description = "this is default vpc"
-    type = "vpc-0d0f87f5d80c69ffa"
+    type = string
+    default = "vpc-0d0f87f5d80c69ffa"
   
 }
