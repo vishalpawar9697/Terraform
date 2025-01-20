04ad2aa61b355d85a01041a2641fe288b8aea631
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-20 23:04:50 +0530

    this is vpc infr terraform file

--- VPC/main.tf
+++ VPC/main.tf
@@ -62,9 +62,9 @@ resource "aws_security_group" "vpc_sg" {
 
     
 ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
