f078ffb9922428bc0d28d7248baf903ca2c9f62e
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-20 23:24:18 +0530

    this is vpc infr terraform file

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,6 +1,6 @@
 # Define the AWS provider
 provider "aws" {
-  region = "us-west-2" # Specify your region
+  region = "us-east-1" # Specify your region
 }
 
 # Create a VPC
@@ -18,7 +18,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-east-1a"
   tags = {
     Name = "public-subnet"
   }
