7c756d2ec790271bd1339d7fe16d27be2660ae9b
vishalpawar9697 <vishalpawar9697@gmail.com>
2025-01-20 23:17:03 +0530

    this is vpc infr terraform file

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,67 +1,65 @@
+# Define the AWS provider
 provider "aws" {
-    region = "us-east-1"
-  
+  region = "us-west-2" # Specify your region
 }
 
+# Create a VPC
 resource "aws_vpc" "my_vpc" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-      Name = "my-vpc"
-    }
-
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my-vpc"
+  }
 }
 
+# Create a public subnet
 resource "aws_subnet" "public_subnet" {
-    vpc_id = "aws_vpc.my_vpc.id"
-    cidr_block = "10.0.1.0/24"
-    tags = {
-      Name = "public-subnet"
-    }  
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2a"
+  tags = {
+    Name = "public-subnet"
+  }
 }
 
-resource "aws_subnet" "privat_subnet" {
-    vpc_id = "aws_vpc.my_vpc.id"
-    cidr_block = "10.0.2.0/24"
-    tags = {
-        Name = "privat-subnet"
-    }  
+# Create an internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "internet-gateway"
+  }
 }
 
-resource "aws_internet_gateway" "my_igw" {
-    vpc_id = "aws_vpc.my_vpc.id"
-    tags = {
-    Name = "my-internet-getway"
-    }
-  
+# Create a route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "public-route-table"
+  }
 }
 
-resource "aws_route_table" "my_route_table" {
-    vpc_id = "aws_vpc.my_vpc.id"
-    tags = {
-        Name = "my-route-table"
-    }  
-}
 # Add a route to the internet gateway in the route table
-
 resource "aws_route" "public_route" {
-    route_table_id = "aws_route_table.my_route_table.id"
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = "aws_internet_getway.my_igw.id"
-
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
+# Associate the route table with the public subnet
 resource "aws_route_table_association" "public_route_table_association" {
-    subnet_id = "aws_subnet.public_subnet.id"
-    route_table_id = "aws_route_table.my_route_table.id"
-  
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_security_group" "vpc_sg" {
-    name = "vpc-sg"
-    vpc_id = "aws_vpc.my_vpc.id"
+# Create a security group
+resource "aws_security_group" "default_sg" {
+  name        = "default-sg"
+  description = "Default security group"
+  vpc_id      = aws_vpc.my_vpc.id
 
-    
-ingress {
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -76,8 +74,6 @@ ingress {
   }
 
   tags = {
-    Name = "vpc-security-group"
+    Name = "default-security-group"
   }
 }
-
-
